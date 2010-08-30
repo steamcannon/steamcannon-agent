1197f6d37fc79ecca6edd8eb2365d59ea50b2ccc
Marek Goldmann <marek.goldmann@gmail.com>
2010-08-30 12:46:42 +0200

    more tests to cover configure command

--- spec/services/jboss_as/commands/configure-command-spec.rb
+++ spec/services/jboss_as/commands/configure-command-spec.rb
@@ -6,8 +6,11 @@ module CoolingTower
     before(:each) do
       @service        = mock( 'Service' )
       @service_helper = mock( ServiceHelper )
+      @db             = mock("DB")
 
       @service.stub!( :service_helper ).and_return( @service_helper )
+      @service.stub!(:db).and_return( @db )
+      @service.stub!(:name).and_return( "jboss-as" )
 
       @service.should_receive(:state).and_return( :stopped )
 
@@ -59,7 +62,7 @@ module CoolingTower
       @cmd.execute( {}.to_json ).should == { :status => "ok", :response => { :state => :stopped } }
     end
 
-    describe ".update" do
+    describe ".configure" do
       it "should do nothing" do
 
         db1 = mock("db1")
@@ -74,7 +77,7 @@ module CoolingTower
 
         @service_helper.should_not_receive( :execute )
 
-        @cmd.configure( {}, "1" )
+        @cmd.configure( {}, "1" ) == true
       end
 
       it "should update gossip host only" do
@@ -94,7 +97,7 @@ module CoolingTower
 
         @service.should_receive(:state=).with(:stopped)
 
-        @cmd.configure( { :gossip_host => "10.1.0.1" }, "1" )
+        @cmd.configure( { :gossip_host => "10.1.0.1" }, "1" ) == true
       end
 
       it "should update gossip host and s3_ping, but restart isn't executed because we're in 'stopped' state" do
@@ -118,7 +121,7 @@ module CoolingTower
 
         @service.should_receive(:state=).ordered.with(:stopped)
 
-        @cmd.configure( { :gossip_host => "10.1.0.1", :s3_ping => { 'access_key' => 'a', 'secret_access_key' => 'b', 'bucket' => 'c'} }, "1" )
+        @cmd.configure( { :gossip_host => "10.1.0.1", :s3_ping => { 'access_key' => 'a', 'secret_access_key' => 'b', 'bucket' => 'c'} }, "1" ) == true
       end
 
       it "should update proxy_list and start JBoss AS" do
@@ -136,10 +139,40 @@ module CoolingTower
 
         @service.should_receive(:state=).ordered.with(:started)
 
-        @cmd.configure( {:proxy_list => { "10.1.0.1" => { :host => "10.1.0.1", :port => 80 } } }, "1" )
+        @cmd.configure( {:proxy_list => { "10.1.0.1" => { :host => "10.1.0.1", :port => 80 } } }, "1" ) == true
+      end
+
+      it "should try to update proxy_list and fail gracefully when start operation on JBoss AS failed" do
+        @service_helper.should_receive( :execute ).with( :start, :event => "1", :background => false ).and_return( :status => 'error' )
+        @db.should_receive( :save_event ).with( :configure, :failed, :msg => "Starting JBoss AS failed, couldn't finish updating JBoss AS" )
+        @service.should_receive(:state=).ordered.with(:stopped)
+
+        @cmd.configure( { :proxy_list => { "10.1.0.1" => { :host => "10.1.0.1", :port => 80 } } }, "1" ).should == false
+      end
+
+      it "should try to update proxy_list and fail gracefully when restart operation on JBoss AS failed" do
+        @service_helper.should_receive( :execute ).with( :start, :event => "1", :background => false ).and_return( :status => 'ok' )
+        @service_helper.should_receive( :execute ).with( :restart, :event => "1", :background => false ).and_return( :status => 'error' )
+
+        proxy_list_cmd = mock(UpdateProxyListCommand)
+        proxy_list_cmd.should_receive( :execute ).with( { "10.1.0.1" => { :host => "10.1.0.1", :port => 80 } } ).and_return( true )
+
+        UpdateProxyListCommand.should_receive(:new).with( :log => @log ).and_return( proxy_list_cmd )
+
+        @db.should_receive( :save_event ).with( :configure, :failed, :msg => "Restarting JBoss AS failed, couldn't finish updating JBoss AS" )
+        @service.should_receive(:state=).ordered.with(:stopped)
+
+        @cmd.configure( { :proxy_list => { "10.1.0.1" => { :host => "10.1.0.1", :port => 80 } } }, "1" ).should == false
+      end
+
+      it "should return false when something bad happens" do
+        @service_helper.should_receive( :execute ).with( :start, :event => "1", :background => false ).and_raise( "Unexpected error" )
+        @db.should_receive( :save_event ).with( :configure, :failed, :msg => "An error occurred while configuring 'jboss-as' service" )
+        @service.should_receive(:state=).ordered.with(:stopped)
+
+        @cmd.configure( { :proxy_list => { "10.1.0.1" => { :host => "10.1.0.1", :port => 80 } } }, "1" ).should == false
       end
     end
-
   end
 end
 
