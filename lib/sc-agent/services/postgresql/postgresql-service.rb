015ed547bf9e41a3c4c4ea8d017879f023c38c93
Marek Goldmann <marek.goldmann@gmail.com>
2010-10-05 19:24:44 +0200

    first cut on postgresql service

---
+++ lib/sc-agent/services/postgresql/postgresql-service.rb
@@ -0,0 +1,107 @@
+#
+# Copyright 2010 Red Hat, Inc.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+require 'sc-agent/managers/service-manager'
+require 'json'
+
+module SteamCannon
+  class PostgreSQLService
+    attr_reader :name
+
+    def initialize(options = {})
+      @db             = ServiceManager.register(self, 'postgresql')
+      @log            = options[:log] || Logger.new(STDOUT)
+      @exec_helper    = options[:exec_helper] || ExecHelper.new({:log => @log})
+      @service_helper = options[:service_helper] || ServiceHelper.new(self, :log => @log)
+
+      @name           = 'postgresql'
+
+      # TODO should we also include :error status?
+      @state          = :started # available statuses: :starting, :started, :configuring, :stopping, :stopped
+    end
+
+    def restart
+      @service_helper.execute(:restart, :backgroud => true)
+    end
+
+    def start
+      @service_helper.execute(:start, :backgroud => true)
+    end
+
+    def stop
+      @service_helper.execute(:stop, :backgroud => true)
+    end
+
+    def configure(data)
+      status
+    end
+
+    def status
+      status_output = @exec_helper.execute("service #{@name} status").match(/(running|stopped)\.*$/)
+
+      @state = :error
+
+      unless status_output.nil?
+        case status_output[1]
+          when 'running'
+            @state = :started
+          when 'stopped'
+            @state = :stopped
+        end
+      end
+
+      {:state => @state}
+    end
+
+    def artifact(artifact_id)
+      begin
+        artifact = @db.artifact(artifact_id.to_i)
+      rescue => e
+        @log.error e
+      end
+
+      unless artifact.nil?
+        {:name => artifact.name, :size => artifact.size, :type => artifact.type}
+      else
+        msg = "Could not retrieve artifact with id = #{artifact_id}"
+        @log.error msg
+        raise msg
+      end
+    end
+
+    def artifacts
+      artifacts = []
+
+      @db.artifacts.each do |artifact|
+        artifacts << {:name => artifact.name, :id => artifact.id}
+      end
+
+      {:artifacts => artifacts}
+    end
+
+    # TODO: dummy output, should be changed in the future
+    def deploy(artifact)
+      status
+    end
+
+    # TODO: dummy output, should be changed in the future
+    def undeploy(artifact_id)
+      status
+    end
+  end
+end
---
+++ spec/services/postgresql/postgresql-service-spec.rb
@@ -0,0 +1,79 @@
+# JBoss, Home of Professional Open Source
+# Copyright 2009, Red Hat Middleware LLC, and individual contributors
+# by the @authors tag. See the copyright.txt in the distribution for a
+# full listing of individual contributors.
+#
+# This is free software; you can redistribute it and/or modify it
+# under the terms of the GNU Lesser General Public License as
+# published by the Free Software Foundation; either version 3 of
+# the License, or (at your option) any later version.
+#
+# This software is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+# Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this software; if not, write to the Free
+# Software Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+# 02110-1301 USA, or see the FSF site: http://www.fsf.org.
+
+require 'sc-agent/services/postgresql/postgresql-service'
+require 'openhash/openhash'
+
+module SteamCannon
+  describe PostgreSQLService do
+
+    before(:each) do
+      @db             = mock(DBHelper)
+      @log            = Logger.new('/dev/null')
+
+      ServiceManager.should_receive(:register).and_return(@db)
+
+      @service        = PostgreSQLService.new(:log => @log)
+      @exec_helper    = @service.instance_variable_get(:@exec_helper)
+      @service_helper = @service.instance_variable_get(:@service_helper)
+    end
+
+    it "should return started status" do
+      @exec_helper.should_receive(:execute).with("service postgresql status").and_return("postmaster (pid 1729 1728 1727 1726 1724 1708) is running...")
+      @service.status.should == {:state => :started}
+    end
+
+    it "should return stopped status" do
+      @exec_helper.should_receive(:execute).with("service postgresql status").and_return("postmaster is stopped")
+      @service.status.should == {:state => :stopped}
+    end
+
+    it "should return error status" do
+      @exec_helper.should_receive(:execute).with("service postgresql status").and_return("bleh blah")
+      @service.status.should == {:state => :error}
+    end
+
+    it "should return status on deploy operation" do
+      @service.should_receive(:status).and_return({:state => :stopped})
+      @service.deploy('something').should == {:state => :stopped}
+    end
+
+    it "should return status on undeploy operation" do
+      @service.should_receive(:status).and_return({:state => :stopped})
+      @service.undeploy('something').should == {:state => :stopped}
+    end
+
+    it "should restart the service" do
+      @service_helper.should_receive(:execute).with(:restart, :backgroud => true).and_return({:state => :restarting})
+      @service.restart.should == {:state => :restarting}
+    end
+
+    it "should stop the service" do
+      @service_helper.should_receive(:execute).with(:stop, :backgroud => true).and_return({:state => :stopping})
+      @service.stop.should == {:state => :stopping}
+    end
+
+    it "should start the service" do
+      @service_helper.should_receive(:execute).with(:start, :backgroud => true).and_return({:state => :starting})
+      @service.start.should == {:state => :starting}
+    end
+  end
+end
+
